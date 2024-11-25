fe6258f5487a1664c44e2f60ed947c791788ecb2
Nono0325 <131783153+Nono0325@users.noreply.github.com>
2024-11-25 14:16:50 +0800

    Add files via upload

---
+++ TcpFileServer/mainwindow.h
@@ -0,0 +1,23 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTabWidget>
+#include "tcpfilesender.h"
+#include "tcpfileserver.h"
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    QTabWidget *tabWidget;       // 用于切换发送和接收功能
+    TcpFileSender *fileSender;   // 发送文件模块
+    TcpFileServer *fileServer;   // 接收文件模块
+};
+
+#endif // MAINWINDOW_H
